e997f491b11738a7ae1eee0d4e3a42e756f49c2d
Mostafa Atef <sasaatefsaad@gmail.com>
2023-10-08 22:58:30 +0300

    Do Controller Section Task

---
+++ Controller_task/Controllers/BankController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Controller_task.Controllers {
+    public class BankController : Controller {
+        [Route("/")]
+        public IActionResult Index() {
+            return Content("Welcome in the bank application", "text/plain");
+        }
+
+        [Route("/account-details")]
+        public IActionResult AccountDetails() {
+
+            var accountDetails = new {
+                accountNumber = 1001,
+                accountHolderName = "Example Name",
+                currentBalance = 5000
+            };
+            return Json(accountDetails);
+        }
+
+        [Route("/account-statement")]
+        public IActionResult AccountStatement() {
+            return File("sample.pdf", "application/pdf");
+        }
+
+        [Route("/get-current-balance/{accountNumber?}")]
+        public IActionResult CurrentBalance(int? accountNumber) {
+            var accountDetails = new {
+                accountNumber = 1001,
+                accountHolderName = "Example Name",
+                currentBalance = 5000
+            };
+            if (int.TryParse(Convert.ToString(accountNumber), out int id)) {
+                if (id != 1001) {
+                    Response.StatusCode = 400;
+                    return Content("Account Number should be 1001", "text/plain");
+                }
+                return Content(accountDetails.currentBalance.ToString(), "text/plain");
+            }
+            Response.StatusCode = 404;
+            return Content("Account Number should be supplied", "text/plain");
+        }
+
+    }
+}
